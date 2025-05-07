3b28296bbb7fa2143c4042cf2cdcde97ccbb7ce8
SinaAb7 <sinaabdollahip@gmail.com>
2025-05-07 07:49:12 +0100

    update shrinkwrap script

--- scripts/install-shrinkwrap.sh
+++ scripts/install-shrinkwrap.sh
@@ -7,7 +7,8 @@ sudo apt-get install git netcat-openbsd python3 python3-pip telnet
 sudo pip3 install pyyaml termcolor tuxmake
 git clone https://git.gitlab.arm.com/tooling/shrinkwrap.git
 
-cp ./shrinkwrap-configs/* ./shrinkwrap/config/.
 cd shrinkwrap
 git checkout -f 31f3ecfb75e83e44e150f65f89049358ce819f0c
 git apply ../patch/shrinkwrap.patch
+cd ..
+cp ./shrinkwrap-configs/* ./shrinkwrap/config/.
